e1b4163e2c332aa24ce27d7ea1b0a77f1713abcd
alefe-redes-computadores <202509217108@alunos.estacio.br>
2026-06-25 14:18:45 -0300

    Atualizar o page.tsx

--- src/app/(app)/more/page.tsx
+++ src/app/(app)/more/page.tsx
@@ -261,7 +261,9 @@ export default function MorePage() {
 
     const canvas = canvasRef.current
     const ctx = canvas.getContext('2d')
-    const img = document.createElement('img')
+    const img = document.createElement('img');
+    img.src = selectedImage!;
+
     img.src = selectedImage!
 
     img.onload = () => {
