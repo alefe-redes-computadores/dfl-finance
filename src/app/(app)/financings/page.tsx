3de274b0dfe8c03a6f75428e302b70e119ec86ef
alefe-redes-computadores <202509217108@alunos.estacio.br>
2026-07-04 18:16:42 -0300

    Atualizar o page.tsx

--- src/app/(app)/financings/page.tsx
+++ src/app/(app)/financings/page.tsx
@@ -22,7 +22,7 @@ import { useHapticFeedback } from "@/hooks/useHapticFeedback"
 import { useLocalData } from "@/hooks/useLocalData"
 import { useLocalSync } from "@/hooks/useLocalSync"
 import { LoadingSkeleton, Skeleton } from "@/components/Skeleton"
-import { useAuth } from "@/contexts/AuthContext"
+import { useAuth } from "@/lib/hooks/useAuth"
 
 type Installment = {
   id: string
