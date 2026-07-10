2a7b93f26ab65084ae07b3810c6fe914557bf260
alefe-redes-computadores <202509217108@alunos.estacio.br>
2026-07-10 17:16:16 -0300

    Criar not-found.tsx

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Página não encontrada</h1>
+      <p>Desculpe, a página que você procura não existe.</p>
+    </div>
+  )
+}
