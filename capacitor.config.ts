1c2764d768712f9715fe8e6f3e2681a9e449e825
alefe-redes-computadores <202509217108@alunos.estacio.br>
2026-07-10 16:39:27 -0300

    Atualizar o capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,12 +3,19 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.dafamilialanches.app',
   appName: 'DFL Finance',
-  webDir: 'out', // 🔥 Aponta para a pasta estática que o Next.js vai gerar
+  webDir: 'out',
   bundledWebRuntime: false,
   server: {
     androidScheme: 'https',
     cleartext: true
-  }
+  },
+  plugins: {
+    GoogleAuth: {
+      serverClientId: '726269935852-l4lu5tqigkvarjb47juqta6ksr9g94eq.apps.googleusercontent.com',
+      scopes: ['profile', 'email'],
+      forceCodeForRefreshToken: true,
+    },
+  },
 };
 
 export default config;
