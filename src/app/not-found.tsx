159a5ff86c078ecd1271efa35c3360a642609058
alefe-redes-computadores <202509217108@alunos.estacio.br>
2026-07-10 18:00:05 -0300

    Excluir not-found.tsx

--- src/app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Página não encontrada</h1>
-      <p>Desculpe, a página que você procura não existe.</p>
-    </div>
-  )
-}
