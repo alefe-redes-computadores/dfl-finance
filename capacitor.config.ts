7004125736a459cbe4b95e5023f0abec77c1f4e7
alefe-redes-computadores <202509217108@alunos.estacio.br>
2026-07-10 17:04:10 -0300

    Atualizar o capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -11,8 +11,8 @@ const config: CapacitorConfig = {
   },
   plugins: {
     GoogleAuth: {
-      serverClientId: '726269935852-l4lu5tqigkvarjb47juqta6ksr9g94eq.apps.googleusercontent.com',
       scopes: ['profile', 'email'],
+      serverClientId: '726269935852-l4lu5tqigkvarjb47juqta6ksr9g94eq.apps.googleusercontent.com',
       forceCodeForRefreshToken: true,
     },
   },
