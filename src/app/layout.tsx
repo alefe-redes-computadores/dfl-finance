fba40993b0fa5dd02e31a3a182c15ec5e5803d27
alefe-redes-computadores <202509217108@alunos.estacio.br>
2026-07-11 08:59:03 -0300

    Atualizar o layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/contexts/ThemeContext'
 import { ToastProvider } from '@/contexts/ToastContext'
+import { DeepLinkListener } from '@/components/DeepLinkListener'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -44,6 +45,7 @@ export default function RootLayout({
       <body className="antialiased bg-gray-50 dark:bg-slate-900">
         <ThemeProvider>
           <ToastProvider>
+            <DeepLinkListener />
             {children}
           </ToastProvider>
         </ThemeProvider>
