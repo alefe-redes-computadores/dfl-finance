df2f6c937583796435b582b511207a023b33f913
alefe-redes-computadores <202509217108@alunos.estacio.br>
2026-07-19 15:21:11 -0300

    Atualizar o page.tsx

--- src/app/(app)/cards/page.tsx
+++ src/app/(app)/cards/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-
 import { useEffect, useState, useRef, useCallback, useMemo } from 'react'
 import { useRouter } from 'next/navigation'
 import { useAuth } from '@/lib/hooks/useAuth'
